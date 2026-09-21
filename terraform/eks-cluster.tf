1359f94fb51c084b5af4632a6931330150be117f
devopsmaster <devopsmaster987@gmail.com>
2026-09-21 08:55:36 +0500

    1

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -10,7 +10,8 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "ami-0b6d9d3d33ba97d99"
+    ami_type = "CUSTOM"
+    ami_id = "ami-0b6d9d3d33ba97d99"
   }
 
   eks_managed_node_groups = {
