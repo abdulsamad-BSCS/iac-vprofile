d8ad2b6e3ce4070aad6e35f2e1727f7803a50e06
devopsmaster <devopsmaster987@gmail.com>
2026-09-21 11:36:19 +0500

    1

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -25,14 +25,6 @@ module "eks" {
       desired_size = 2
     }
 
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.large"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
+   
   }
 }
