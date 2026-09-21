2091e97bcd44d245d071936d8883ee30e586dcff
devopsmaster <devopsmaster987@gmail.com>
2026-09-21 08:49:22 +0500

    5

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -4,14 +4,13 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
-  
+
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+    ami_type = "AL2023_x86_64_STANDARD"
   }
 
   eks_managed_node_groups = {
